5caca9f05897b21e30bd4a613e7382fc96b7970b
Diego Go <dgomes@Diegos-MacBook-Pro.local>
2022-02-13 18:14:24 -0300

    inciando jogo da forca

--- py/2/forca.py
+++ py/2/forca.py
@@ -3,6 +3,22 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = 'musica'
+
+    enforcou = False
+    acerto = False
+
+    # precisa ser true para parar
+    while not enforcou and not acerto:
+        chute = input('Qual letra ? ')
+
+        for letra in palavra_secreta:
+            if chute == letra:
+                print(chute)
+
+
+        print('Continua ... ')
+
     print("Fim do jogo")
 
 
