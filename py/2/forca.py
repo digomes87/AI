24dbf56529bf8bf414aa573dee4bbacdf1497e08
Diego Go <dgomes@Diegos-MacBook-Pro.local>
2022-03-03 20:05:58 -0300

    Jogo implementado com leitura de arquivo externo

--- py/2/forca.py
+++ py/2/forca.py
@@ -1,25 +1,53 @@
+import random
+
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = 'musica'
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]  # list Comprehensions
 
     enforcou = False
-    acerto = False
+    acertou = False
+    erros = 0
 
+    print(letras_acertadas)
     # precisa ser true para parar
-    while not enforcou and not acerto:
+    while not enforcou and not acertou:
         chute = input('Qual letra ? ')
+        chute = chute.strip().upper()
 
-        for letra in palavra_secreta:
-            if chute == letra:
-                print(chute)
+        index = 0
 
+        if chute in palavra_secreta:
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
-        print('Continua ... ')
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
-    print("Fim do jogo")
+    if acertou:
+        print("Venceu o Game")
+    else:
+        print("Tente Novamente !")
+
+    print("Fim de Jogo")
 
 
 if __name__ == "__main__":
